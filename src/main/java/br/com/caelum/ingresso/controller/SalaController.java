2d7f324e18f15817a911d95a514e2e4698ca8481
Diego <diegozanni@gmail.com>
2018-06-30 16:48:48 -0300

    Usando o JUNIT e criando o gerenciadorSessao

--- src/main/java/br/com/caelum/ingresso/controller/SalaController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SalaController.java
@@ -3,6 +3,7 @@ package br.com.caelum.ingresso.controller;
 import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Sala;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
